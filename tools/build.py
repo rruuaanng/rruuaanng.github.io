f8cea670b711d1a05e179c8c8847c6ecb7a5f815
ruang <longjinyii@outlook.com>
2024-10-03 18:01:03 +0800

    build.py

--- tools/build.py
+++ tools/build.py
@@ -1,5 +1,4 @@
 import os
-
 import frontmatter
 from string import Template
 from markdown import markdown
@@ -12,52 +11,51 @@ notedir = '../notes'
 notes = [f'{notedir}/{note}'
          for note in os.listdir(notedir)]
 
+# site title
+site_title = 'RUANG site'
+
 # index template
 index_tmpl = '''<!DOCTYPE html>
 <html>
-
 <head>
-    <title>RUANG site</title>
-    <style>
-        body {
-            background-color:whitesmoke
-        }
-        .article-panel {
-            background-color: #f9f9f9;
-            padding: 20px;
-            border-radius: 5px;
-            box-shadow: 0 2px 5px rgba(0,0,0,0.1);
-            margin-bottom: 20px;
-        }
-        .article-title {
-            font-size: 1.5em;
-            margin-bottom: 10px;
-        }
-        .article-date {
-            color: #777;
-            margin-bottom: 10px;
-        }
-        .article-content {
-            line-height: 1.6;
-        }
-    </style>
+<title>$site_title</title>
+<style>
+body {
+background-color:whitesmoke
+}
+.article-panel {
+background-color: #f9f9f9;
+padding: 20px;
+border-radius: 5px;
+box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+margin-bottom: 20px;
+}
+.article-title {
+font-size: 1.5em;
+margin-bottom: 10px;
+}
+.article-date {
+color: #777;
+margin-bottom: 10px;
+}
+.article-content {
+line-height: 1.6;
+}
+</style>
 </head>
-
 <body>
-    <h1 style="text-align: left;">RUANG site</h1>
-
-    <div class="row1">
-        <p style="text-align: left;">
-            <a href="http://www.github.com/rruuaanng"><b>github</b></a> |
-            <a href="notes/"><b>notes</b></a> |
-        </p>
-    </div>
-
-    <hr><div class="blog">
-        <ul>
-            $news_list
-        </ul>
-    </div><hr>
+<h1 style="text-align: left;">$site_title</h1>
+<div class="row1">
+<p style="text-align: left;">
+<a href="http://www.github.com/rruuaanng"><b>github</b></a> |
+<a href="notes/"><b>notes</b></a> |
+</p>
+</div>
+<hr><div class="blog">
+<ul>
+$news_list
+</ul>
+</div><hr>
 </body>
 </html>
 '''
@@ -86,7 +84,8 @@ def render_news(post_meta, content):
 def render_index(news_list):
     '''render index'''
     template = Template(index_tmpl)
-    return template.substitute(news_list=news_list)
+    return template.substitute(site_title=site_title,
+                               news_list=news_list)
 
 def md_converto_html(md_text):
     '''markdown convert to html'''
@@ -94,7 +93,6 @@ def md_converto_html(md_text):
 
 def main():
     news_list = ''
-
     for note in notes:
         with open(note, 'r') as fp:
             try:
